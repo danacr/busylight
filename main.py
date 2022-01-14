efd4e33ba62e6d7d10256a939b0133083b364507
danacr <dan@acristinii.com>
2022-01-14 23:45:57 +0100

    Let's try

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, Response, request, redirect, url_for
+from busylight.lights.embrava import Blynclight
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/green/", methods=['POST'])
+def green():
+    light = Blynclight.first_light()
+
+    light.on((255, 255, 255))
+    return render_template('index.html');
+
+@app.route("/off/", methods=['POST'])
+def off():
+    light = Blynclight.first_light()
+
+    light.off()
+    return render_template('index.html');
+
+if __name__ == '__main__':
+   app.run(debug = True)
